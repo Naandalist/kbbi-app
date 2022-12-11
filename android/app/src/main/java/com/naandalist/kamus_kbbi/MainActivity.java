2c8423c11028a1fc51311968271b84facee23a66
“naandalist” <listiananda.apriliawan@gmail.com>
2022-12-12 00:42:37 +0700

    chore: update package name

--- android/app/src/main/java/com/naandalist/kamus_kbbi/MainActivity.java
+++ android/app/src/main/java/com/naandalist/kamus_kbbi/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kamus_kbbi;
+package com.naandalist.kamus_kbbi;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "kamus_kbbi";
+    return "KamusKBBI";
   }
 
   /**
