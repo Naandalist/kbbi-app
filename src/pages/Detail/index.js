be58245dd5e351f02f688c63c09f745c064c6d52
Naandalist <listiananda.apriliawan@gmail.com>
2025-06-26 08:36:47 +0700

    refactor: simplify component structure and improve data handling in Detail page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,312 +7,201 @@ import {
   ScrollView,
   SafeAreaView,
 } from 'react-native';
-import {useDispatch} from 'react-redux';
 import {
-  sanitizeJsonResponse,
-  showError,
-  removeTrailingDigits as sanitizeText,
   getData,
+  showError,
   fonts,
   colors,
+  removeSuperscriptDigits,
 } from '../../utils';
 import {Gap} from '../../components';
 import Icon from 'react-native-vector-icons/Ionicons';
+import SkeletonLoader from './SkeletonLoader';
 
 const initialData = {
-  istilah: '',
-  entri: '',
-  bentukTidakBaku: '',
-  kataTurunan: [],
-  gabunganKata: [],
-  peribahasa: [],
-  etimologi: {
-    bahasaAsal: '',
-    arti: '',
-    kataAsal: '',
-  },
-  data: [
+  word: '',
+  authenticated: false,
+  entries: [
     {
-      lema: '',
-      arti: [
+      id: '',
+      nama: '',
+      nomor: '',
+      makna: [
         {
-          kelas_kata: '',
-          deskripsi: '',
+          nomor: '',
+          kelasKata: [
+            {
+              kode: '',
+              nama: '',
+            },
+          ],
+          definisi: '',
+          contoh: [],
         },
       ],
+      terkait: {},
     },
   ],
 };
 
-// Header Component
-const Header = ({onPress, pressedWord}) => {
-  return (
-    <View style={styles.header}>
-      <View style={styles.backButtonContainer}>
-        <TouchableOpacity onPress={onPress}>
-          <View style={styles.arrowContainer}>
-            <Icon name="close-outline" size={30} color="black" />
-          </View>
-        </TouchableOpacity>
-        <Text
-          numberOfLines={1}
-          ellipsizeMode="tail"
-          style={styles.backText}>{`Hasil pencarian: ${pressedWord}`}</Text>
-      </View>
-    </View>
-  );
-};
-
-// Card Section Component
-const CardSection = ({children}) => {
-  return (
-    <View style={styles.cardSection}>
-      <View style={styles.bodySection}>{children}</View>
-    </View>
-  );
-};
-
-// Info Section Component
-const InfoSection = ({label, exponent, content}) => {
-  return (
-    <View style={styles.infoSection}>
-      {label && (
-        <View style={styles.wrapperLabelText}>
-          <Text style={styles.labelText}>{label}</Text>
-          {exponent && (
-            <Text style={styles.exponentText}>{` ${exponent}`}</Text>
-          )}
+const Header = ({onPress, pressedWord}) => (
+  <View style={styles.header}>
+    <View style={styles.backButtonContainer}>
+      <TouchableOpacity onPress={onPress}>
+        <View style={styles.arrowContainer}>
+          <Icon name="close-outline" size={30} color="black" />
         </View>
-      )}
-      {content && (
-        <Text selectable style={[styles.contentText, styles.boldText]}>
-          {content}
-        </Text>
-      )}
+      </TouchableOpacity>
     </View>
-  );
-};
+  </View>
+);
 
-// Meaning Item Component
-const MeaningItem = ({index, partOfSpeech, description, etimo}) => {
-  return (
-    <View style={styles.meaningContainer}>
-      <Text style={styles.indexText}>{index}. </Text>
-      <View>
-        {etimo ? (
-          <>
-            <Text selectable style={styles.partOfSpeechText}>
-              {partOfSpeech}{' '}
-            </Text>
-            <Gap height={3} />
-            <Text selectable style={styles.descriptionText}>
-              {description}{' '}
-            </Text>
-          </>
-        ) : (
-          <>
-            <Text selectable style={styles.descriptionText}>
-              {description}{' '}
-            </Text>
-            <Gap height={3} />
-            <Text selectable style={styles.partOfSpeechText}>
-              {partOfSpeech}{' '}
-            </Text>
-          </>
-        )}
+const CardSection = ({children}) => (
+  <View style={styles.cardSection}>
+    <View style={styles.bodySection}>{children}</View>
+  </View>
+);
+
+const InfoSection = ({label, exponent, content, hasBorder}) => (
+  <View style={[styles.infoSection, hasBorder && styles.dashedBorder]}>
+    {label && (
+      <View style={styles.wrapperLabelText}>
+        <Text style={styles.labelText}>{label}</Text>
+        <Text style={styles.exponentText}>{exponent}</Text>
       </View>
-    </View>
-  );
-};
+    )}
+    {content && typeof content === 'string' && (
+      <Text selectable style={[styles.contentText, styles.boldText]}>
+        {removeSuperscriptDigits(content)}
+      </Text>
+    )}
 
-// Bullet Item Component
-const BulletItem = ({title, content, isArray = false}) => {
-  return (
-    <View style={styles.meaningContainer}>
-      <Text style={styles.indexText}>- </Text>
+    {content && typeof content === 'object' && (
       <View>
-        <Text selectable style={styles.partOfSpeechText}>
-          {title}
-        </Text>
-        <Gap height={3} />
-        {isArray && content.length > 1 ? (
-          content.map((text, index) => (
-            <Text selectable key={index} style={styles.descriptionText}>
-              {index + 1}. {text}.
-            </Text>
-          ))
-        ) : (
-          <Text selectable style={styles.descriptionText}>
-            {content}{' '}
-          </Text>
-        )}
+        <Gap height={5} />
+        <View style={styles.labelsContainer}>
+          {content.kelasKata.map((kelas, i) => (
+            <View key={i} style={styles.labelWrapper}>
+              <Text
+                style={
+                  styles.partOfSpeechText
+                }>{`${kelas.kode} ${kelas.nama}`}</Text>
+            </View>
+          ))}
+        </View>
       </View>
+    )}
+  </View>
+);
+
+const MeaningItem = ({index, exponent, description, labels, examples}) => (
+  <View style={styles.meaningContainer}>
+    <View>
+      <View style={styles.rowCenter}>
+        <Text style={styles.labelText}>definisi </Text>
+        <Text style={styles.exponentText}>{exponent}</Text>
+      </View>
+      <Text style={styles.descriptionText}>{description}</Text>
+      {examples
+        .filter(example => example.teks.length > 1)
+        .map((example, i) => (
+          <View key={i} style={styles.exampleWrapper}>
+            {example.teks && (
+              <View style={styles.exampleRow}>
+                <Text style={styles.exampleText}>contoh </Text>
+                <Icon
+                  name="arrow-forward-outline"
+                  size={13}
+                  color="black"
+                  style={styles.exampleIcon}
+                />
+                <Text style={styles.exampleText}>{`${example.teks}`}</Text>
+              </View>
+            )}
+          </View>
+        ))}
     </View>
-  );
-};
+  </View>
+);
 
-// Main Detail Component
-export default function Detail({route, navigation}) {
-  const dispatch = useDispatch();
-  const [wordDetail, setWordDetail] = useState(initialData);
-  const [showMore, setShowMore] = useState(false);
-
+export default function Detail(props) {
+  const {route, navigation} = props;
   const {pressedWord, selectedLetter} = route.params;
 
-  const getDataFromKBBI = useCallback(async () => {
-    try {
-      // Set loading state before API call
-      dispatch({type: 'SET_LOADING', value: true});
+  const [wordDetail, setWordDetail] = useState(initialData);
 
+  useEffect(() => {
+    const fetchData = async () => {
       try {
         const response = await getData(selectedLetter, pressedWord);
-
         if (response.error) {
           showError('Kata yang dicari tidak ditemukan dalam KBBI.');
         } else if (response.message === 'Fetch Success') {
-          // Sanitize and set the word details
-          setWordDetail(sanitizeJsonResponse(response.data));
+          setWordDetail(response.data);
         }
-
-        return response;
-      } catch (apiError) {
-        console.error('API Error:', JSON.stringify(apiError));
-
+      } catch (error) {
         showError('Terjadi kesalahan saat mengambil data dari KBBI.');
-        return null;
-      } finally {
-        dispatch({type: 'SET_LOADING', value: false});
+        console.error(error);
       }
-    } catch (error) {
-      console.error('Unexpected error:', error);
-      showError('Terjadi kesalahan.');
+    };
 
-      dispatch({type: 'SET_LOADING', value: false});
-      return null;
-    }
-  }, [dispatch, selectedLetter, pressedWord]);
+    fetchData();
+  }, [pressedWord, selectedLetter]);
 
-  useEffect(() => {
-    getDataFromKBBI();
-  }, [getDataFromKBBI]);
-
-  const isDataEmpty = data => {
-    return data?.length < 1;
-  };
-
-  const isEtimologyExist = () => {
+  if (wordDetail.authenticated === false) {
     return (
-      wordDetail?.etimologi?.bahasaAsal !== '' &&
-      wordDetail?.etimologi?.arti !== '' &&
-      wordDetail?.etimologi?.kataAsal !== ''
+      <>
+        <Header pressedWord={pressedWord} onPress={() => navigation.goBack()} />
+        <SkeletonLoader />
+      </>
     );
-  };
-
-  const isAdditionalInfoExist = () => {
-    return (
-      !isDataEmpty(wordDetail?.bentukTidakBaku) ||
-      !isDataEmpty(wordDetail?.peribahasa) ||
-      !isDataEmpty(wordDetail?.kataTurunan) ||
-      !isDataEmpty(wordDetail?.gabunganKata)
-    );
-  };
+  }
 
   return (
     <SafeAreaView style={styles.page}>
       <Header pressedWord={pressedWord} onPress={() => navigation.goBack()} />
-
       <ScrollView>
-        {!isDataEmpty(wordDetail?.data[0].arti[0].deskripsi) &&
-          wordDetail?.data.map((item, index) => (
-            <CardSection key={index}>
-              <InfoSection
-                label="Kata dasar"
-                exponent={index + 1}
-                content={sanitizeText(item.lema)}
-              />
+        <CardSection>
+          <InfoSection label="kata dasar" content={wordDetail.word} />
+          {wordDetail.entries[0]?.nama !== wordDetail.word && (
+            <InfoSection
+              label="ejaan"
+              content={wordDetail.entries[0]?.nama || ''}
+            />
+          )}
+          {wordDetail.entries[0]?.rootWord && (
+            <InfoSection
+              label="akar kata"
+              content={wordDetail.entries[0]?.rootWord || ''}
+            />
+          )}
+        </CardSection>
+        {wordDetail.entries.length > 0 ? (
+          wordDetail.entries.map((entry, idx) => (
+            <CardSection key={entry.id || idx}>
+              {entry.makna[0]?.kelasKata?.length > 0 && (
+                <InfoSection
+                  label="kelas kata"
+                  exponent={wordDetail.entries.length > 1 ? idx + 1 : null}
+                  content={entry.makna[0]}
+                  hasBorder
+                />
+              )}
 
-              {item.arti.map((arti, i) => (
+              {entry.makna.map((maknaObj, i) => (
                 <MeaningItem
-                  key={i}
-                  index={index + 1}
-                  partOfSpeech={arti.kelas_kata}
-                  description={arti.deskripsi}
+                  key={maknaObj.nomor || i}
+                  index={maknaObj.nomor || i + 1}
+                  exponent={wordDetail.entries.length > 1 ? idx + 1 : null}
+                  labels={maknaObj.kelasKata}
+                  description={maknaObj.definisi}
+                  examples={maknaObj.contoh}
                 />
               ))}
             </CardSection>
-          ))}
-
-        {/* Etymology section */}
-        {isEtimologyExist() && (
-          <CardSection>
-            <InfoSection
-              label={'Hasil studi penelusuran asal-usul kata\nsecara etimologi'}
-            />
-            <MeaningItem
-              index="1"
-              partOfSpeech="Sumber bahasa asalnya"
-              description={wordDetail?.etimologi.bahasaAsal}
-              etimo={true}
-            />
-            <MeaningItem
-              index="2"
-              partOfSpeech="Arti, makna, atau terjemahnya"
-              description={wordDetail?.etimologi.arti}
-              etimo={true}
-            />
-            <MeaningItem
-              index="3"
-              partOfSpeech="Kata asal dari bahasa aslinya"
-              description={wordDetail?.etimologi.kataAsal}
-              etimo={true}
-            />
-          </CardSection>
-        )}
-
-        {/* Additional information section */}
-        {showMore && isAdditionalInfoExist() && (
-          <CardSection>
-            <InfoSection />
-
-            {!isDataEmpty(wordDetail?.bentukTidakBaku) && (
-              <BulletItem
-                title="Bentuk tidak baku"
-                content={sanitizeText(wordDetail?.bentukTidakBaku)}
-              />
-            )}
-
-            {!isDataEmpty(wordDetail?.peribahasa) && (
-              <BulletItem
-                title="Peribahasa"
-                content={wordDetail?.peribahasa}
-                isArray
-              />
-            )}
-
-            {!isDataEmpty(wordDetail?.kataTurunan) && (
-              <BulletItem
-                title="Kata turunan"
-                content={wordDetail?.kataTurunan}
-                isArray
-              />
-            )}
-
-            {!isDataEmpty(wordDetail?.gabunganKata) && (
-              <BulletItem
-                title="Gabungan Kata "
-                content={wordDetail?.gabunganKata}
-                isArray
-              />
-            )}
-          </CardSection>
-        )}
-
-        {isAdditionalInfoExist() && wordDetail !== initialData && (
-          <TouchableOpacity onPress={() => setShowMore(!showMore)}>
-            <Text style={styles.showMoreText}>
-              {`Tampilkan Lebih ${showMore ? 'Sedikit' : 'Banyak'}`}
-            </Text>
-          </TouchableOpacity>
+          ))
+        ) : (
+          <Text style={styles.emptyText}>Tidak ada hasil ditemukan.</Text>
         )}
       </ScrollView>
     </SafeAreaView>
@@ -320,7 +209,6 @@ export default function Detail({route, navigation}) {
 }
 
 const styles = StyleSheet.create({
-  // Layout styles
   page: {
     flex: 1,
     backgroundColor: colors.page,
@@ -330,19 +218,9 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: colors.page,
   },
-  cardSection: {
-    backgroundColor: colors.white,
-    paddingBottom: 20,
-    marginTop: 10,
-  },
-  bodySection: {
-    backgroundColor: colors.white,
-    paddingHorizontal: 16,
-    paddingTop: 10,
-  },
-
   backButtonContainer: {
     paddingVertical: 10,
+    backgroundColor: colors.white,
   },
   arrowContainer: {
     marginTop: 20,
@@ -356,14 +234,23 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginLeft: 16,
   },
-
-  // Word info styles
-  infoSection: {
-    marginBottom: 10,
+  cardSection: {
+    backgroundColor: colors.white,
+    paddingBottom: 10,
+    marginTop: 10,
+  },
+  bodySection: {
+    paddingHorizontal: 16,
+    paddingTop: 10,
+  },
+  dashedBorder: {
     borderBottomWidth: 1,
     borderBottomColor: 'grey',
     borderStyle: 'dashed',
     paddingBottom: 10,
+  },
+  infoSection: {
+    marginBottom: 10,
   },
   wrapperLabelText: {
     flexDirection: 'row',
@@ -381,34 +268,65 @@ const styles = StyleSheet.create({
   },
   contentText: {
     color: 'black',
-    fontSize: 18,
-    fontFamily: fonts.primary[600],
+    fontFamily: fonts.primary[700],
   },
   boldText: {
     fontFamily: fonts.primary[700],
   },
-
-  // Definition styles
-  meaningContainer: {
+  labelsContainer: {
     flexDirection: 'row',
-    paddingTop: 20,
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    alignContent: 'flex-start',
+    marginTop: 5,
   },
-  indexText: {
-    fontFamily: fonts.primary_italic[400],
-    fontStyle: 'italic',
-    color: 'rgba(30, 39, 46,0.7)',
+  labelWrapper: {
+    alignSelf: 'flex-start',
+    flexDirection: 'row',
+    backgroundColor: '#e4eff9',
+    borderRadius: 4,
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    overflow: 'hidden',
+    marginRight: 8,
+    marginBottom: 6,
   },
   partOfSpeechText: {
     fontFamily: fonts.primary_italic[400],
     color: 'rgba(30, 39, 46,0.7)',
   },
+  meaningContainer: {
+    flexDirection: 'row',
+    paddingBottom: 10,
+  },
   descriptionText: {
     fontFamily: fonts.primary[700],
     color: 'black',
   },
-  showMoreText: {
+  exampleWrapper: {
+    marginTop: 8,
+  },
+  exampleRow: {
+    flexDirection: 'row',
+    alignItems: 'start',
+  },
+  exampleIcon: {
+    top: 4,
+    marginRight: 5,
+  },
+  exampleText: {
+    fontFamily: fonts.primary[400],
+    color: 'black',
+    maxWidth: '85%',
+  },
+  emptyText: {
     color: 'grey',
     textAlign: 'center',
     marginVertical: 20,
+    fontFamily: fonts.primary[600],
+  },
+  rowCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
