cf199d0ae0450fa8326444b13eb24f2c5af8302d
Naandalist <listiananda.apriliawan@gmail.com>
2025-11-09 18:39:00 +0700

    chore: implement rozenite

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import 'react-native-gesture-handler';
+import {useNetworkActivityDevTools} from '@rozenite/network-activity-plugin';
+import {usePerformanceMonitorDevTools} from '@rozenite/performance-monitor-plugin';
+import {useReactNavigationDevTools} from '@rozenite/react-navigation-plugin';
+
 import {NavigationContainer} from '@react-navigation/native';
 import FlashMessage from 'react-native-flash-message';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {StyleSheet} from 'react-native';
 import Router from './router';
+import {useRef} from 'react';
 
 export default function App() {
   return <MainApp />;
 }
 
 function MainApp() {
+  const navigationRef = useRef(null);
+  useReactNavigationDevTools({ref: navigationRef});
+
+  useNetworkActivityDevTools();
+  usePerformanceMonitorDevTools();
+
   return (
     <GestureHandlerRootView style={styles.container}>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" duration={3000} />
