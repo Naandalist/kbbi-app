c5b169b8f9d04a1a136c56f218280885b300d22a
Naandalist <listiananda.apriliawan@gmail.com>
2025-12-04 07:04:26 +0700

    chore: update version to 7.0.1 and remove jail-monkey dependency

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import FlashMessage from 'react-native-flash-message';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {StyleSheet} from 'react-native';
 import Router from './router';
-import {useRef, useEffect} from 'react';
-import JailMonkey from 'jail-monkey';
+import {useRef} from 'react';
 
 export default function App() {
   return <MainApp />;
@@ -21,17 +20,6 @@ function MainApp() {
 
   useNetworkActivityDevTools();
   usePerformanceMonitorDevTools();
-
-  useEffect(() => {
-    if (!__DEV__) {
-      const isJailBroken = JailMonkey.isJailBroken();
-      const isDebuggedMode = JailMonkey.isDebuggedMode();
-
-      if (isJailBroken || isDebuggedMode) {
-        navigationRef.current?.navigate('SecurityWarning');
-      }
-    }
-  }, []);
 
   return (
     <GestureHandlerRootView style={styles.container}>
